b08e957f97f9b2c61a2ee02ca565c9c11209cb06
Joe Golton <joebuyer@manycycles.com>
2016-04-07 12:47:56 -0700

    Initial commit

---
+++ twentysixteen-child-filterjoe/functions.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Twenty Sixteen Child FilterJoe functions and definitions
+ *
+ * When using a child theme you can override certain functions (those wrapped
+ * in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before
+ * the parent theme's file, so the child theme functions would be used.
+ *
+ * @link https://codex.wordpress.org/Theme_Development
+ * @link https://codex.wordpress.org/Child_Themes
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are
+ * instead attached to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters,
+ * {@link https://codex.wordpress.org/Plugin_API}
+ */
+
+/**
+ * Twenty Sixteen only works in WordPress 4.4 or later.
+ */
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+function jptweak_remove_share() {
+    remove_filter( 'the_content', 'sharing_display',19 );
+    remove_filter( 'the_excerpt', 'sharing_display',19 );
+    if ( class_exists( 'Jetpack_Likes' ) ) {
+        remove_filter( 'the_content', array( Jetpack_Likes::init(), 'post_likes' ), 30, 1 );
+    }
+}
+ 
+add_action( 'loop_start', 'jptweak_remove_share' );
+
+function my_theme_add_editor_styles() {
+    add_editor_style( 'custom-editor-style.css' );
+}
+add_action( 'admin_init', 'my_theme_add_editor_styles' );
+
+if ( ! function_exists( 'twentysixteen_entry_date' ) ) :
+/**
+ * Prints HTML with date information for current post.
+ *
+ * This twentysixteen_entry_date() function overrides the twentysixteen default.
+ * CSS is actually where I unhid "updated". However, that left an ugly mess which I fixed
+ * here by putting in a more compact date format n/j/y, putting in date labels
+ * (Published: , Updated ), and putting in a space hard-space space between Published and Updated.
+ * @since Twenty Sixteen 1.0
+ */
+function twentysixteen_entry_date() {
+	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+		$time_string = '<time class="entry-date published" datetime="%1$s">Published: %2$s</time><time class="updated" datetime="%3$s"> &nbsp; Updated: %4$s</time>';
+	}
+
+	$time_string = sprintf( $time_string,
+		esc_attr( get_the_date( 'c' ) ),
+		get_the_date( 'n/j/y' ),
+		esc_attr( get_the_modified_date( 'c' ) ),
+		get_the_modified_date( 'n/j/y' )
+	);
+
+	printf( '<span class="posted-on"><span class="screen-reader-text">%1$s </span><a href="%2$s" rel="bookmark">%3$s</a></span>',
+		_x( 'Posted on', 'Used before publish date.', 'twentysixteen' ),
+		esc_url( get_permalink() ),
+		$time_string
+	);
+}
+endif;
